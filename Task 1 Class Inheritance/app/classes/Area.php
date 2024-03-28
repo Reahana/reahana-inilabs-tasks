db5729e49416298e108a0c31c89a3637f5de8c85
Reahana <reahana233@gmail.com>
2024-03-28 14:13:51 +0600

    Area class

---
+++ Task 1 Class Inheritance/app/classes/Area.php
@@ -0,0 +1,14 @@
+<?php
+
+
+namespace App\classes;
+
+
+class Area extends GeometricShapes
+{
+ public function area()
+ {
+     echo 'Area of circle is ' . $this->circle(2) . '<br> Area of rectangle is ' . $this->rectangle(5,3) ;
+
+ }
+}
